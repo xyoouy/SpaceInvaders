5157b256117c0934e050a38138165d8767e340c1
xyoouy <egorloparev@gmail.com>
2022-12-16 19:55:29 +0500

    Add Simple Animation

--- Enemy.py
+++ Enemy.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 from datetime import datetime
 from Bullet import Bullet
@@ -5,7 +7,7 @@ from Enums import Color, Direction, BulletType
 
 
 class Enemy:
-    speed = 0.5
+    speed = 7.5
     bullets = list()
     scale = 3
 
@@ -13,25 +15,39 @@ class Enemy:
         self.x = x
         self.y = y
         self.shoot_timer = 0
-        self.shoot_timer_length = 1
+        self.shoot_timer_length = random.randint(2, 6)
+        self.current_sprite = 1
         self.dead = False
         self.color = Color.Green.value
-        self.sprite = pygame.transform.scale(pygame.image.load("Sprites/Enemy.png"), (11 * self.scale, 8 * self.scale))
+        self.sprite = pygame.transform.scale(pygame.image.load(f"Sprites/Enemy{self.current_sprite}green.png"),
+                                             (11 * self.scale, 8 * self.scale))
         self.rect = self.sprite.get_rect().move((x, y))
         self.direction = Direction.LEFT
+        self.move_timer = 0
+        self.move_timer_length = 1
 
     def move(self, window_width):
         if self.dead:
             return
+        if abs(self.move_timer_length - self.move_timer) >= 0.01:
+            self.move_timer += 0.013
+            return
+
         if self.x >= 0 and self.direction == Direction.LEFT:
             self.x -= self.speed
             if self.x < 0:
                 self.direction = Direction.RIGHT
         if self.x <= window_width - self.rect.width and self.direction == Direction.RIGHT:
+            print("move right")
             self.x += self.speed
             if self.x > window_width - self.rect.width:
                 self.direction = Direction.LEFT
 
+        self.current_sprite = (self.current_sprite + 1) % 2
+        self.sprite = pygame.transform.scale(pygame.image.load(f"Sprites/Enemy{self.current_sprite}green.png"),
+                                             (11 * self.scale, 8 * self.scale))
+
+        self.move_timer = 0
         self.rect.x = self.x
 
     def draw(self, window):
@@ -42,11 +58,15 @@ class Enemy:
         if len(self.bullets) < 5 and self.shoot_timer >= self.shoot_timer_length:
             self.bullets.append(Bullet(self.x, self.y + 10 * self.scale, self.color, BulletType.Enemy))
             self.shoot_timer = 0
+            self.shoot_timer_length = random.randint(2, 6)
 
     def is_colliding(self, bullet):
-        if bullet.bullet_type == BulletType.Enemy:
-            return False
+        if self.dead:
+            return
 
-        return self.rect.colliderect(bullet.rect)
+        if not bullet.bullet_type == BulletType.Enemy:
+            self.dead = self.rect.colliderect(bullet.rect)
+
+        return self.dead
 
 
